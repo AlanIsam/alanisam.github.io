457ebcadc729560af49d8aadbf463e4882be2561
Alan <alanisam66@gmail.com>
2026-07-07 12:58:59 +0800

    revamped personal pages with projects

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const typed = new Typed('#element', {
-    strings: ['Hello, my name is <b>Alan Isam</b> <br> I am an aspiring Game Developer and Programmer!'],
-    typeSpeed: 40,
+    strings: [
+      "Hi, I'm <b>Alan Isam</b>",
+      'I build <b>full-stack apps</b>.',
+      'I build <b>2D games</b>.',
+    ],
+    typeSpeed: 45,
+    backSpeed: 25,
+    backDelay: 1400,
+    loop: true,
   });
